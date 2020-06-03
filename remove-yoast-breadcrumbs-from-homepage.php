439ee8a1348a8d0efb9b2de4a61e52872866d82b
Keith <keith@keith-driscoll.com>
2020-06-03 09:14:54 -0400

    Create remove-yoast-breadcrumbs-from-homepage.php

---
+++ remove-yoast-breadcrumbs-from-homepage.php
@@ -0,0 +1,14 @@
+/* Remove Yoast breadcrumbs from homepage only
+*
+*  @link https://digitalgeekblog.com/remove-yoast-breadcrumbs-from-homepage-only
+*
+*/
+
+add_filter('wpseo_breadcrumb_single_link', 'dgb_remove_homepage_breadcrumb' );
+
+function dgb_remove_homepage_breadcrumb( $link_output) {
+	if( is_front_page() ) {
+		$link_output = '';
+	}
+	return $link_output;	   
+}
